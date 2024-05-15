e303a1c19809bc7c2b71242429896b5900566834
ColombIATeam <163057153+ColombIATeam@users.noreply.github.com>
2024-05-15 10:27:54 -0500

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -12,5 +12,5 @@ def read_item(item_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    #uvicorn.run(app, host="127.0.0.1", port=8080)
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    #uvicorn.run(app)
